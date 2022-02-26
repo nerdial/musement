3ab1faaf97849d8af7b557ec9f360a0b8bb6d4ea
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-02-26 16:44:35 +0330

    Implemented a console application to stdout all cities with weather, also created a unit test for the command.

--- src/Command/CrawlerCommand.php
+++ src/Command/CrawlerCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\CityService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,32 +10,32 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CrawlerCommand extends Command
 {
     // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'app:crawler';
+    protected static $defaultName = 'app:crawl';
+
+    public function __construct(private CityService $cityService, string $name = null)
+    {
+        parent::__construct($name);
+    }
 
     protected function configure(): void
     {
-        // ...
+        $this
+            ->setHelp('This command calls forecast api 
+            and print the weather of each city into stdout');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('it is working fine');
+        $cities = $this->cityService->getAllCitiesWithForecast();
 
-        // ... put here the code to create the user
+        foreach ($cities as $city) {
+            $cityName = $city['name'];
+            $today = $city['days'][0];
+            $tomorrow = $city['days'][0];
+            $output->writeln("<info>Processed city $cityName | $today - $tomorrow </>");
+            $output->writeln('-------------------------------------------------------');
+        }
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
 }
---
+++ tests/Unit/CrawlerCommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class CrawlerCommandTest extends KernelTestCase
+{
+    protected ArrayCollection $citiesWithForecastCached;
+
+    public function setUp(): void
+    {
+        $this->citiesWithForecastCached = new ArrayCollection([
+            [
+                'name' => 'Tehran',
+                'days' => new ArrayCollection(['Sunny', 'Sunny']),
+            ],
+            [
+                'name' => 'London',
+                'days' => new ArrayCollection(['Moderate rain', 'Sunny']),
+            ],
+        ]);
+
+        parent::setUp();
+    }
+
+    public function testIfCommandPrintsCorrectly(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $cache = $kernel->getContainer()->get('cache.app');
+
+        $cache->delete('cities');
+
+        $cache->get('cities', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+
+            return $this->citiesWithForecastCached;
+        });
+
+        $command = $application->find('app:crawl');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $output = $commandTester->getDisplay();
+
+        $this->assertStringContainsString('Processed city Tehran | Sunny - Sunny', $output);
+
+        $this->assertStringContainsString('Processed city London | Moderate rain - Moderate rain',
+            $output);
+
+        $commandTester->assertCommandIsSuccessful();
+    }
+}
